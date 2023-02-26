65b452a627d39328ab2cb29fc59765111570e128
xHieu21 <90350383+xHieu21@users.noreply.github.com>
2023-02-26 03:31:45 -0600

    Moved spreadsheets operations
    Moved it to a component

--- src/App.tsx
+++ src/App.tsx
@@ -3,58 +3,20 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import React from "react";
 import { NavBar } from "./components/nav-bar/nav-bar";
-import "@silevis/reactgrid";
-import { ReactGrid , Column , Row } from "@silevis/reactgrid";
 import { Spreadsheet } from "./components/spreadsheet/spreadsheet";
 
-interface Person {
-  name: string;
-  surname: string;
-}
 
-const getPeople = (): Person[] => [
-  { name: "Thomas", surname: "Goldman" },
-  { name: "Susie", surname: "Quattro" },
-  { name: "", surname: "" }
-];
 
-const getColumns = (): Column[] => [
-  { columnId: "name", width: 150 },
-  { columnId: "surname", width: 150 }
-];
-
-const headerRow: Row = {
-  rowId: "header",
-  cells: [
-    { type: "header", text: "Name" },
-    { type: "header", text: "Surname" }
-  ]
-};
-
-const getRows = (people: Person[]): Row[] => [
-  headerRow,
-  ...people.map<Row>((person, idx) => ({
-    rowId: idx,
-    cells: [
-      { type: "text", text: person.name },
-      { type: "text", text: person.surname }
-    ]
-  }))
-];
 function App() {
+  
   const [count, setCount] = useState(0);
-  const [people] = React.useState<Person[]>(getPeople());
-
-  const rows = getRows(people);
-  const columns = getColumns();
-
   
   return (
     <div className="App">
       <NavBar />
       <Spreadsheet />
       <div>
-        <ReactGrid rows={rows} columns={columns} />;
+        <button>Add Cells</button>  
         <a href="https://vitejs.dev">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
--- src/components/spreadsheet/spreadsheet.tsx
+++ src/components/spreadsheet/spreadsheet.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import * as React from "react";
+import { Workbook } from "@fortune-sheet/react";
+import "@fortune-sheet/react/dist/index.css";
 import { ReactGrid, Column, Row } from "@silevis/reactgrid";
 import "@silevis/reactgrid/styles.css";
 
 
 export const Spreadsheet = () => {
+  
 
   return (
-    <div> </div>);
+    
+    <div><style>
+    html, body, #root 
+    &lbrace;
+      width: 100%;
+      height: 100%;
+    &rbrace; 
+    </style>
+    <div id="root"></div> </div>);
 };
