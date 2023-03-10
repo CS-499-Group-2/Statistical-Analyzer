818fc67a2c41f11653f48a8b3c43c1ace070f1c9
Bernard Allotey <benard.allotey4@gmail.com>
2023-03-10 15:49:05 -0600

    STL-47 Adds a way to bind data from the app component to the spreadsheet component.
    - Added a formula engine to the spreadsheet
    - Now binds based on initial data passed to it
    - Calls a callback whenever the spreadsheet data changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import { NavBar } from "./components/nav-bar/nav-bar";
 import { Spreadsheet } from "./components/spreadsheet/spreadsheet";
 import { Operation, operations } from "./stats/operations";
-
+import { CsvData } from "./file-handling/import";
 
 
 function App() {
   const [selectedOperations, setSelectedOperations] = React.useState<string[]>([]);
+  // This is the source of truth for the data. We will try to pass this to all of the operations that need it.
+  const [data, setData] = React.useState<CsvData>({data: [[10, 15], [1, 2], [5, 10]], headers: ["Column 1", "Column 2"]});
   
   // This is the useEffect hook. It is called whenever the things in the array change. In this case, we want to log the selected operations whenever they change. This will be called after re-rendering, so the state will have changed
   React.useEffect(() => {
@@ -23,11 +25,29 @@ function App() {
     // We can't print the selected operations here, because the site hasn't re-rendered yet, so the state hasn't changed
   };
 
+  /**
+   * Called whenever a cell in the spreadsheet changes. When this happens, we need to update the data in our state, since it doesn't automatically update.
+   * @param row The row of the cell that changed
+   * @param column The column of the cell that changed
+   * @param value The new value of the cell
+   */
+  const onCellChange = (row: number, column: number, value: number) => {
+    setData(previousData => {
+      const newData = {...previousData}; // We need to clone the data, because we can't mutate the state directly
+      if (newData.data[row] === undefined) { // If row doesn't exist, create a new row
+        newData.data[row] = [];
+      }
+      newData.data[row][column] = value; // Update the value
+      console.log("New data: ", newData);
+      return newData;
+    });
+  };
+
   return (
     <div className="App">
       <NavBar availableOperations={[...operations] /* For some reason, operations is readonly, so we just clone it here*/} 
         onOperationSelected={onOperationSelected} />
-      <Spreadsheet />
+      <Spreadsheet data={data} onCellChange={onCellChange} />
     </div>
   );
 }
--- src/components/spreadsheet/spreadsheet.tsx
+++ src/components/spreadsheet/spreadsheet.tsx
@@ -1,34 +1,62 @@
-import { useState } from "react";
 import * as React from "react";
-import Handsontable from "handsontable";
 import { registerAllModules } from "handsontable/registry";
 import "handsontable/dist/handsontable.full.css";
 import { HotTable } from "@handsontable/react";
 import "handsontable/dist/handsontable.full.min.css";
 import "./spreadsheet.css";
+import { CsvData } from "../../file-handling/import";
+import { HyperFormula } from "hyperformula";
 
 registerAllModules();
-export const Spreadsheet = () => {
-  
+
+/**
+ * The properties to pass to the spreadsheet component
+ */
+export interface SpreadsheetProps {
+  /** The data to be shown  */
+  data: CsvData,
+  onCellChange?: (row: number, column: number, value: number) => void
+}
+
+export const Spreadsheet = (props: SpreadsheetProps) => {  
 
   return (
     
     <div className="sheet">
       <HotTable
-        data={[
-          ["", "", "", "", "", ""],
-          ["", "", "", "", "", ""],
-          ["", "", "", "", "", ""],
-          ["", "", "", "", "", ""]
-        ]}
+        data={props.data.data}
         rowHeaders={true}
-        colHeaders={true}
+        colHeaders={props.data.headers}
+        formulas={{
+          engine: HyperFormula,
+        }}
         width="100%"
         height="100%"
         rowHeights={23}
+        type="numeric"
         colWidths={100}
         contextMenu={["row_above", "row_below", "remove_row" , "col_left", "col_right", "remove_col"]}
         licenseKey="non-commercial-and-evaluation" // for non-commercial use only
+        validator={"numeric"}
+        beforeChange={(changes) => {
+          // With the beforeChange callback, we can prevent the change from happening. We do this by returning false. See: https://handsontable.com/docs/react-data-grid/api/hooks/#beforechange
+          // We want to prevent the change if any of the new values are not numbers
+          if (changes.some(([row, column, oldValue, newValue]) => isNaN(parseFloat(newValue)))) return false;
+          // We don't need the old value
+          changes?.forEach(([row, column, oldValue, newValue]) => {
+            let columnNumber: number;
+            // For some reason, the column can be a string or a number. I don't know why, but this is a workaround
+            if (typeof column === "string") {
+              columnNumber = props.data.headers.indexOf(column);
+            } else {
+              columnNumber = column;
+            }
+            // Convert the value to a number
+            const newValueAsNumber = parseFloat(newValue);
+            // Call the onCellChange callback if it exists
+            props.onCellChange?.(row, columnNumber, newValueAsNumber);
+          });
+        }}
       />
     </div>);
 };
