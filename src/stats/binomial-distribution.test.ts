a0cc9450f7a306993b89222260143485730f9ba2
GabrielDorsey <gcd0003@uah.edu>
2023-04-01 14:57:51 -0500

    test

--- src/stats/binomial-distribution.test.ts
+++ src/stats/binomial-distribution.test.ts
@@ -6,12 +6,12 @@ test("should calculate binomial distribution", () => {
   const binomialDistribution = calculateBinomialDistribution(5, 0.5);
   expect(binomialDistribution).toEqual({
   
-    "0": 0.03125,
-    "1": 0.15625,
-    "2": 0.3125,
-    "3": 0.3125,
-    "4": 0.15625,
-    "5": 0.03125,
+    0: 0.03125,
+    1: 0.15625,
+    2: 0.3125,
+    3: 0.3125,
+    4: 0.15625,
+    5: 0.03125,
 
 
   });
