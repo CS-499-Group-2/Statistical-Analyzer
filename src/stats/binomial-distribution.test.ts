9d19fbc07c97de13307d3443529e49fbec2b751d
GabrielDorsey <gcd0003@uah.edu>
2023-04-01 15:00:25 -0500

    fixing from object to array

--- src/stats/binomial-distribution.test.ts
+++ src/stats/binomial-distribution.test.ts
@@ -4,17 +4,7 @@ import { calculateBinomialDistribution } from "./calculations";
 
 test("should calculate binomial distribution", () => {
   const binomialDistribution = calculateBinomialDistribution(5, 0.5);
-  expect(binomialDistribution).toEqual({
-  
-    0: 0.03125,
-    1: 0.15625,
-    2: 0.3125,
-    3: 0.3125,
-    4: 0.15625,
-    5: 0.03125,
-
-
-  });
+  expect(binomialDistribution).toEqual([0.03125, 0.15625, 0.3125, 0.3125, 0.15625, 0.03125]);
 });
 
 
