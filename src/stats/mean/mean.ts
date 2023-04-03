18f27406eee3095350eefd99b7ac97bf9f681012
Gabriel Dorsey <gcd0003@uah.edu>
2023-04-03 16:39:39 -0500

    fixed indentation issues

--- src/stats/mean/mean.ts
+++ src/stats/mean/mean.ts
@@ -8,41 +8,41 @@ interface Inputs {
 
 
 export const Mean: Operation<Inputs> = {
-    name: "Mean",
-    onSelected: (selectedCellsByColumn, spreadsheet, inputs): Result[] => {
-        const meanColor = inputs["Color of the mean line"] as string;
+  name: "Mean",
+  onSelected: (selectedCellsByColumn, spreadsheet, inputs): Result[] => {
+    const meanColor = inputs["Color of the mean line"] as string;
 
-        //Validation
-        for (const column of selectedCellsByColumn) {
-            if (column.values.length < 1) {
-                alert("Please select at One Cell.");
-                return [];
-            }
-        }
+    //Validation
+    for (const column of selectedCellsByColumn) {
+      if (column.values.length < 1) {
+        alert("Please select at One Cell.");
+        return [];
+      }
+    }
         
-        //For each column, calculate the mean and return a result
-        return selectedCellsByColumn.map((column) => {
-            const meanValue = calculateMean(column.values);
-            const title = `${column.name} | Mean`;
-            return {
-                name : title,
-                values: [meanValue],
-                graphs: [{
-                    chartType: "Horizontal Bar",
-                    data: [{
-                       value : meanValue,
-                        color : meanColor,
-                        label : "Mean",
+    //For each column, calculate the mean and return a result
+    return selectedCellsByColumn.map((column) => {
+      const meanValue = calculateMean(column.values);
+      const title = `${column.name} | Mean`;
+      return {
+        name : title,
+        values: [meanValue],
+        graphs: [{
+          chartType: "Horizontal Bar",
+          data: [{
+            value : meanValue,
+            color : meanColor,
+            label : "Mean",
 
-                    }],
-                }], 
-            };
-        });
-    },
-    isValid: (selectedCellsByColumn): boolean => {
-        return selectedCellsByColumn.length !== 0;
-    }, 
-    keys : { 
-        "Color of the mean line" : "Text", 
-    },
+          }],
+        }], 
+      };
+    });
+  },
+  isValid: (selectedCellsByColumn): boolean => {
+    return selectedCellsByColumn.length !== 0;
+  }, 
+  keys : { 
+    "Color of the mean line" : "Text", 
+  },
 };
