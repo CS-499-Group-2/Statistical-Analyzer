71a36db29716e4dde20c17be06041fe9a91160df
Bernard Allotey <benard.allotey4@gmail.com>
2023-04-17 14:37:19 -0500

    Format docuent

--- src/stats/binomial-distribution/binomial-distribution.ts
+++ src/stats/binomial-distribution/binomial-distribution.ts
@@ -1,17 +1,16 @@
 import { Result, TypedOperation } from "../operation";
 import { calculateBinomialDistribution } from "../calculations";
 
-
 //Defining the inputs
 interface Inputs {
-    "Number of Trials": number;
-    "Probability": number;
+  "Number of Trials": number;
+  Probability: number;
 }
 
 export const BinomialDistribution: TypedOperation<Inputs> = {
   name: "Binomial Distribution",
   type: "Typed",
-  onSelected: (selectedCellsByColumn, spreadsheet, inputs): Result[] => { 
+  onSelected: (selectedCellsByColumn, spreadsheet, inputs): Result[] => {
     const numberOfTrials = inputs["Number of Trials"] as number;
     const probability = inputs["Probability"] as number;
 
@@ -29,28 +28,29 @@ export const BinomialDistribution: TypedOperation<Inputs> = {
     const binomialDistribution = calculateBinomialDistribution(numberOfTrials, probability);
 
     //Return the result
-    return [{
-      name: "Binomial Distribution",
-      values: binomialDistribution,
-      graphs: [{
-        chartType: "Horizontal Bar",
-        data: binomialDistribution.map((value) => {
-          return {
-            value: value,
-            label: value.toString(),
-          };
-        }),
-      }],
-    }];
+    return [
+      {
+        name: "Binomial Distribution",
+        values: binomialDistribution,
+        graphs: [
+          {
+            chartType: "Horizontal Bar",
+            data: binomialDistribution.map(value => {
+              return {
+                value: value,
+                label: value.toString(),
+              };
+            }),
+          },
+        ],
+      },
+    ];
   },
   isValid: (selectedCellsByColumn): boolean => {
     return selectedCellsByColumn.length !== 0;
   },
   keys: {
     "Number of Trials": "Number",
-    "Probability": "Number",
-        
-  }
-
-
+    Probability: "Number",
+  },
 };
