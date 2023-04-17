1c834e2e2daaf1e6867ad78a7b99ecd16ed3e742
Bernard Allotey <benard.allotey4@gmail.com>
2023-04-17 14:36:27 -0500

    Fix operations

--- src/stats/mean/mean.tsx
+++ src/stats/mean/mean.tsx
@@ -1,8 +1,8 @@
 import { Operation , OperationProps} from "../operation";
 import { calculateMean } from "../calculations";
 import { Graph } from "../../components/graph-display/graphs";
-import React from "react";
-import { Modal, Button, ColorInput  , Select } from "@mantine/core";
+import React, { useEffect } from "react";
+import { Modal, Button, ColorInput, Select } from "@mantine/core";
 
 //Defining the inputs
 const MeanComponent = (props: OperationProps) => {
@@ -12,23 +12,24 @@ const MeanComponent = (props: OperationProps) => {
   const columnNames = selectedCellsByColumn.map(column => column.name);
   const [selectedGraph, setSelectedGraph] = React.useState<string | null>(null);
 
+  useEffect(() => {
+    setSelectedGraph("Horizontal Bar");
+  }, []);
+
   // Called when the user clicks submit
   const onSubmit = () => {
-
-    // Check the graph that the user selecte
-    let graphType;
+    // Check the graph that the user selected
+    let graphType: "Horizontal Bar" | "Vertical Bar" | "Pie" | undefined = undefined;
     switch (selectedGraph) {
-    case "Horizontal Bar":
-      graphType = "Horizontal Bar";
-      break;
-    case "Vertical Bar":
-      graphType = "Vertical Bar";
-      break;
-    case "Pie":
-      graphType = "Pie";
-      break;
-      
-
+      case "Horizontal Bar":
+        graphType = "Horizontal Bar";
+        break;
+      case "Vertical Bar":
+        graphType = "Vertical Bar";
+        break;
+      case "Pie":
+        graphType = "Pie";
+        break;
     }
     // The actual means for each column
     selectedCellsByColumn.forEach(column => {
@@ -42,6 +43,11 @@ const MeanComponent = (props: OperationProps) => {
       });
     });
 
+    // If the user didn't select a graph, we don't need to do anything else
+    if (graphType == undefined) {
+      deselect();
+      return;
+    }
     // The graph to display
     const graph: Graph = {
       chartType: graphType,
@@ -55,7 +61,7 @@ const MeanComponent = (props: OperationProps) => {
     };
     addResult({
       name: `Mean Graphs | ${columnNames.join(", ")}`,
-      values: [ ],
+      values: [],
       graphs: [graph],
     });
     deselect();
@@ -70,7 +76,7 @@ const MeanComponent = (props: OperationProps) => {
         ) => (
           <ColorInput
             key={columnName}
-            label={ " Bar Color for : " + columnName}
+            label={" Bar Color for : " + columnName}
             value={barColors.get(columnName)}
             onChange={newColor => {
               // We need to create a new map because we have to use a different object to force a rerender
@@ -83,20 +89,16 @@ const MeanComponent = (props: OperationProps) => {
       )}
       <Select
         data={[
-          { value: " Horizontal Bar", label: "Horizontal Bar" },
+          { value: "Horizontal Bar", label: "Horizontal Bar" },
           { value: "Vertical Bar", label: "Vertical Bar" },
           { value: "Pie", label: "Pie" },
-
-
         ]}
-        label = "Select the graph you want to display"
+        label="Select the graph you want to display"
         placeholder="Select graph"
         value={selectedGraph}
-        onChange = {setSelectedGraph}
+        onChange={setSelectedGraph}
       />
       <Button onClick={onSubmit}>Submit</Button>
-
-
     </Modal>
   );
 };
--- src/stats/mode/mode.tsx
+++ src/stats/mode/mode.tsx
@@ -1,8 +1,8 @@
 import { Operation ,  OperationProps} from "../operation";
 import { calculateMode } from "../calculations";
 import { Graph } from "../../components/graph-display/graphs";
-import React from "react";
-import { Modal, Button, ColorInput  , Select } from "@mantine/core";
+import React, { useEffect } from "react";
+import { Modal, Button, ColorInput, Select } from "@mantine/core";
 
 //Defining the inputs
 const ModeComponent = (props: OperationProps) => {
@@ -12,21 +12,24 @@ const ModeComponent = (props: OperationProps) => {
   const columnNames = selectedCellsByColumn.map(column => column.name);
   const [selectedGraph, setSelectedGraph] = React.useState<string | null>(null);
 
+  useEffect(() => {
+    setSelectedGraph("Horizontal Bar");
+  }, []);
+
   // Called when the user clicks submit
   const onSubmit = () => {
-
     // Check the graph that the user selecte
-    let graphType;
+    let graphType: "Horizontal Bar" | "Vertical Bar" | "Pie" | undefined = undefined;
     switch (selectedGraph) {
-    case "Horizontal Bar":
-      graphType = "Horizontal Bar";
-      break;
-    case "Vertical Bar":
-      graphType = "Vertical Bar";
-      break;
-    case "Pie":
-      graphType = "Pie";
-      break;
+      case "Horizontal Bar":
+        graphType = "Horizontal Bar";
+        break;
+      case "Vertical Bar":
+        graphType = "Vertical Bar";
+        break;
+      case "Pie":
+        graphType = "Pie";
+        break;
     }
     // The actual modes for each column
     selectedCellsByColumn.forEach(column => {
@@ -40,6 +43,10 @@ const ModeComponent = (props: OperationProps) => {
       });
     });
 
+    if (graphType === undefined) {
+      deselect();
+      return;
+    }
     // The graph to display
     const graph: Graph = {
       chartType: graphType,
@@ -53,7 +60,7 @@ const ModeComponent = (props: OperationProps) => {
     };
     addResult({
       name: `Mode Graphs | ${columnNames.join(", ")}`,
-      values: [ ],
+      values: [],
       graphs: [graph],
     });
     deselect();
@@ -68,7 +75,7 @@ const ModeComponent = (props: OperationProps) => {
         ) => (
           <ColorInput
             key={columnName}
-            label={ "Bar Color for : " + columnName}
+            label={"Bar Color for : " + columnName}
             value={barColors.get(columnName)}
             onChange={newColor => {
               // We need to create a new map because we have to use a different object to force a rerender
@@ -81,20 +88,16 @@ const ModeComponent = (props: OperationProps) => {
       )}
       <Select
         data={[
-          { value: " Horizontal Bar", label: "Horizontal Bar" },
+          { value: "Horizontal Bar", label: "Horizontal Bar" },
           { value: "Vertical Bar", label: "Vertical Bar" },
           { value: "Pie", label: "Pie" },
-
-
         ]}
-        label = "Select the graph you want to display"
+        label="Select the graph you want to display"
         placeholder="Select graph"
         value={selectedGraph}
-        onChange = {setSelectedGraph}
+        onChange={setSelectedGraph}
       />
       <Button onClick={onSubmit}>Submit</Button>
-
-
     </Modal>
   );
 };
