e3807dcd3d41b6e1da9228175be46e4b7f10cd55
Bernard Allotey <benard.allotey4@gmail.com>
2023-04-17 19:44:46 -0500

    Add fix for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: "jsdom"
-  }
+    environment: "jsdom",
+    setupFiles: ["./src/test-setup.ts"],
+  },
 });
